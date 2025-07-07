85145a24a3284bcdf2cc7eea4eb96fcf297bf3bf
Adityapatil1930 <adibpatil1111@gmail.com>
2025-07-07 16:15:34 +0530

    Datatypes in  js

---
+++ 04Datatype.js
@@ -0,0 +1,16 @@
+var a=11211323232323;
+console.log(a);
+
+var b=2372838293272839283n;
+console.log(b);
+
+var c="aditya patil"
+console.log(c)
+
+var c="aditya patil"
+console.log(`my name is :${c}`);
+
+var c="aditya patil"
+console.log('my name is :${c}');
+
+
